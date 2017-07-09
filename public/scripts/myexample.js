4cd62973cb9f0ff9e2cfd16368b6abb86ab78f91
Thought_Nibbler <teaja.hal@gmail.com>
2017-07-09 15:34:32 +0900

    チュートリアルに沿ってコメント欄もどきを作った

--- public/scripts/myexample.js
+++ public/scripts/myexample.js
@@ -1,38 +1,116 @@
 var CommentBox = React.createClass({
-  render: function() {
-    return (
-        <div className="commentBox">
-          <h1>Comments</h1>
-          <CommentForm />
-          <hr />
-          <CommentList />
-        </div>
-    );
-  }
-});
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url      : this.props.url,
+            dataType : 'json',
+            cache    : false,
+            success  : function(data) {
+                this.setState({ data : data });
+            }.bind(this),
+            error    : function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    onCommentSubmit: function(comment) {
+        var comments = this.state.data.slice();
+        comments.push(comment);
+        this.setState({ data : comments });
 
-var CommentList = React.createClass({
-  render: function() {
-    return (
-      <div className="commentList">
-        Hello, world! I am a CommentList.
-      </div>
-    );
-  }
+        $.ajax({
+            url      : this.props.url,
+            dataType : 'json',
+            type     : 'POST',
+            data     : comment,
+            success  : function(data) {
+                this.setState({ data : data });
+            }.bind(this),
+            error    : function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return { data : [] };
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+    render: function() {
+        return (
+            <div className="commentBox">
+              <h1>Comments</h1>
+              <CommentForm onCommentSubmit={this.onCommentSubmit} />
+              <hr />
+              <CommentList data={this.state.data} />
+            </div>
+        );
+    }
 });
 
 var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
+    onSubmit: function(e) {
+        e.preventDefault();
+
+        var author = ReactDOM.findDOMNode(this.refs.author).value.trim();
+        var text   = ReactDOM.findDOMNode(this.refs.text).value.trim();
+
+        if ((!author) || (!text)) {
+            return;
+        }
+
+        var id = new Date().getTime().toString(16);
+        var comment = { author : author, text : text, id : id };
+
+        this.props.onCommentSubmit(comment);
+
+        ReactDOM.findDOMNode(this.refs.author).value = '';
+        ReactDOM.findDOMNode(this.refs.text).value   = '';
+
+        return;
+    },
+    render: function() {
+        return (
+            <div className="commentForm">
+              <input type="text" placeholder="Your name" ref="author" />
+              <input type="text" placeholder="Say something..." ref="text" />
+              <input type="button" value="Post" onClick={this.onSubmit} />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var comments = this.props.data.map(function(data) {
+            return(
+                <Comment author={data.author} text={data.text} key={data.id} />
+            );
+        }).reverse();
+        return (
+            <div className="commentList">
+              {comments}
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function() {
+        return(
+            <div className="comment">
+              <h2 className="commentAuthor">
+                {this.props.author}
+              </h2>
+              {this.props.text}
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+    <CommentBox url="/api/comments" pollInterval={2000} />,
+    document.getElementById('content')
 );
 
